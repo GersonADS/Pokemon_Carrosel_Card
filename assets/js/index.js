a36c9362794551cd9deac2530879060dc8622329
GersonADS <shiranuime@gmail.com>
2023-04-23 22:44:53 -0300

    finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,36 @@
 const btnAvancar = document.querySelector("#btn-avancar");
+const cartoes = document.querySelectorAll(".cartao");
+
+let cartaoAtual = 0;
+
+function esconderCartao(){
+    const cartaoSelecionado = document.querySelector('.selecionado');
+    cartaoSelecionado.classList.remove('selecionado');
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado");
+}
 
 btnAvancar.addEventListener("click", function () {
-  const cartoes = document.querySelectorAll(".cartao");
-  console.log(cartoes[1])
+
+  esconderCartao();
+    
+  cartaoAtual++;
+  if (cartaoAtual === 3) cartaoAtual = 0;
+  mostrarCartao(cartaoAtual);
+
+});
+
+
+const btnVoltar = document.querySelector("#btn-voltar");
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) cartaoAtual = 3;
+
+  esconderCartao();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+
 });
