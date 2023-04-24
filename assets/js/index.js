316f671dad85a7b25ca4b9d1797c91d5fbe9f834
GersonADS <shiranuime@gmail.com>
2023-04-23 22:11:20 -0300

    js

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+const btnAvancar = document.querySelector("#btn-avancar");
+
+btnAvancar.addEventListener("click", function () {
+  const cartoes = document.querySelectorAll(".cartao");
+  console.log(cartoes[1])
+});
